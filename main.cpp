d10669068f46877b3659d8ae7191fcfb3c37fc51
a01704231 <112184077+a01704231@users.noreply.github.com>
2022-09-09 08:50:28 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,10 +11,16 @@
 using namespace std;
 
 int main() {
+	vector <string> v;
+	string k;
 	ifstream archivoLeer;
 	string texto;
     	archivoLeer.open("input1.txt");
-    	getline(archivoLeer,texto)
+    	getline(archivoLeer,texto);
+	k=texto;
+	while(getline(archivoLeer,texto);){
+		v.push_back(texto);	
+	}
 	archivoLeer.close();
 	return 0;
 }
